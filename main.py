667c7024fecff67abcf603efa93e88e5b20d2752
PHEyejixHello <123708464+PHEyejixHello@users.noreply.github.com>
2024-06-26 23:00:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -41,69 +41,70 @@ client_received_data = defaultdict(lambda: defaultdict(bool))
 last_received_packet = defaultdict(lambda: defaultdict(bytes))
 
 last_01_packet_time = {}
+
+
 def is_valid_packet(data, addr):
     ip, port = addr
     current_time = time.time()
 
     # 检查数据包的长度
     if len(data) < 3:
-        #logging.warning(f"Received packet from {addr} is too short, data discarded.")
+        # logging.warning(f"Received packet from {addr} is too short, data discarded.")
         return False
 
     # 检查数据包的类型
     packet_type = data[0]
     if packet_type not in list(range(256)):  # 允许所有类型的数据包
-        #logging.warning(f"Received packet from {addr} with invalid type {packet_type}, data discarded.")
+        # logging.warning(f"Received packet from {addr} with invalid type {packet_type}, data discarded.")
         return False
 
-    if packet_type == [0x1c,0x06,0x08,0x10] : return False # 服务器数据包
+    if packet_type == [0x1c, 0x06, 0x08, 0x10]: return False  # 服务器数据包
 
     # 检查数据包的Magic字段
     if packet_type in [0x01, 0x05, 0x07]:
         if MAGIC.search(data) is None:
-            #logging.warning(f"Received packet from {addr} does not contain valid Magic, data discarded.")
+            # logging.warning(f"Received packet from {addr} does not contain valid Magic, data discarded.")
             return False
-       
-    #交手包顺序验证
+
+    # 交手包顺序验证
 
     if packet_type == 0x05:
         connection_check[ip][port] = 1
-        
+
     if packet_type == 0x07 and connection_check[ip][port] == 2:
         connection_check[ip][port] = 3
     elif packet_type == 0x07 and connection_check[ip][port] != 2:
         connection_check[ip][port] = 0
-
-
 
     # 更新连接状态
     if connection_check[ip][port] == 4:  # 如果是"Open Connection Request 1"
         connection_check[ip][port] = 0
         tmp = connection_states
         if any(state == 1 for state in tmp[ip].values()):
-            #logging.warning(f"Received 'Open Connection Request 1' from {addr} but IP {ip} is already connected, data discarded.")
+            # logging.warning(f"Received 'Open Connection Request 1' from {addr} but IP {ip} is already connected, data discarded.")
             return False
         connection_states[ip][port] = 1
-        
+
     # 检查连接状态
 
-    if connection_states[ip][port] == 0 and packet_type not in [0x01, 0x05, 0x07,0x09]:  # 如果还没有发送"Open Connection Request 1"
-        logging.warning(f"Received packet from {addr} before 'Open Connection Request 1', data discarded.")
+    if connection_states[ip][port] == 0 and packet_type not in [0x01, 0x05, 0x07,
+                                                                0x09]:  # 如果还没有发送"Open Connection Request 1"
+        #logging.warning(f"Received packet from {addr} before 'Open Connection Request 1', data discarded.")
         return False
 
     # 检查数据包是否为线性相关
     if last_received_packet[ip][port] == data:
-        #logging.warning(f"Received duplicate packet from {addr}, data discarded.")
+        # logging.warning(f"Received duplicate packet from {addr}, data discarded.")
         return False
-    
+
     if packet_type == 0x01:
         last_send_time = last_01_packet_time.get(ip, 0)
         time_since_last_send = current_time - last_send_time
         if time_since_last_send < 1:  # 如果距离上次发送不足1秒
-            logging.warning(f"Client {ip} attempted to send more than one 0x01 packet within a second, discarding.")
+            #logging.warning(f"Client {ip} attempted to send more than one 0x01 packet within a second, discarding.")
             return False
         last_01_packet_time[ip] = current_time  # 更新最后发送0x01类型数据包的时间
-    
+
     # 更新最后交互时间
     last_interaction_time[ip][port] = time.time()
 
@@ -113,9 +114,11 @@ def is_valid_packet(data, addr):
     # 如果数据包是有效的，返回True
     return True
 
+
 def reset_packet_counts():
     packet_counts.clear()
     threading.Timer(1, reset_packet_counts).start()
+
 
 def check_disconnections():
     current_time = time.time()
@@ -139,6 +142,7 @@ def check_disconnections():
                     del last_received_packet[ip]
     threading.Timer(1, check_disconnections).start()
 
+
 def print_stats(stats):
     tmp = stats
     for ip, data in tmp.items():
@@ -147,7 +151,8 @@ def print_stats(stats):
     stats.clear()
     threading.Timer(5, print_stats, args=(stats,)).start()
 
-def forward_data(source_socket, target_address, stats, target_port ,source_port):
+
+def forward_data(source_socket, target_address, stats, target_port, source_port):
     source_socket.setblocking(0)  # 设置为非阻塞模式
 
     addr_map = {}  # 用于存储每个客户端的地址和对应的转发套接字
@@ -161,12 +166,12 @@ def forward_data(source_socket, target_address, stats, target_port ,source_port)
                 try:
                     data, addr = source_socket.recvfrom(65536)  # 将缓冲区大小设置为65536字节
                     if len(data) > MAX_PACKET_SIZE:
-                        #logging.warning("Received data is larger than buffer size, data discarded.")
+                        # logging.warning("Received data is larger than buffer size, data discarded.")
                         continue
                     if not is_valid_packet(data, addr):
                         continue
                     if packet_counts[addr[0]] >= MAX_PACKETS_PER_SECOND:
-                        #logging.warning(f"Received too many packets from {addr[0]}, data discarded.")
+                        # logging.warning(f"Received too many packets from {addr[0]}, data discarded.")
                         continue
                     packet_counts[addr[0]] += 1
                     stats[addr[0]]['recv'] += len(data)
@@ -181,27 +186,23 @@ def forward_data(source_socket, target_address, stats, target_port ,source_port)
                 try:
                     data, addr = sock.recvfrom(65536)  # 将缓冲区大小设置为65536字节
                     if len(data) > MAX_PACKET_SIZE:
-                        #logging.warning("Received data is larger than buffer size, data discarded.")
+                        # logging.warning("Received data is larger than buffer size, data discarded.")
                         continue
                     if data[0] == 0x1c:
-                        data = data.replace(target_port.encode('utf-8') ,source_port.encode('utf-8'))
-
-                    if data[0] == 0x0E:
-                        print(1)
-                        continue
+                        data = data.replace(target_port.encode('utf-8'), source_port.encode('utf-8'))
 
                     tmp2 = list(addr_map.items())
                     client_addr = [k for k, v in tmp2 if v == sock][0]
                     stats[client_addr[0]]['sent'] += len(data)
                     source_socket.sendto(data, client_addr)  # 将数据发送回原来的客户端
 
-                    #交手包顺序验证
-                        
+                    # 交手包顺序验证
+
                     if data[0] == 0x06 and connection_check[client_addr[0]][client_addr[1]] == 1:
                         connection_check[client_addr[0]][client_addr[1]] = 2
                     elif data[0] == 0x06 and connection_check[client_addr[0]][client_addr[1]] == 1:
                         connection_check[client_addr[0]][client_addr[1]] = 0
-                        
+
                     if data[0] == 0x08 and connection_check[client_addr[0]][client_addr[1]] == 3:
                         connection_check[client_addr[0]][client_addr[1]] = 4
                     elif data[0] == 0x08 and connection_check[client_addr[0]][client_addr[1]] != 3:
@@ -210,6 +211,7 @@ def forward_data(source_socket, target_address, stats, target_port ,source_port)
                     client_received_data[client_addr[0]][client_addr[1]] = True  # 标记客户端已接收到数据
                 except BlockingIOError:
                     continue  # 没有数据可读
+
 
 def main():
     try:
@@ -226,7 +228,8 @@ def main():
         stats = defaultdict(lambda: defaultdict(int))  # 使用defaultdict来存储每个IP的数据量
 
         logging.info(f"开始转发：{source_port} -> {target_port}")
-        forward_thread = threading.Thread(target=forward_data, args=(server_socket, target_address, stats,target_port,source_port))
+        forward_thread = threading.Thread(target=forward_data,
+                                          args=(server_socket, target_address, stats, target_port, source_port))
         forward_thread.start()
 
         threading.Timer(5, print_stats, args=(stats,)).start()  # 每5秒打印一次统计信息
@@ -240,6 +243,7 @@ def main():
     except Exception as e:
         logging.error(f"发生错误：{e}")
         # 继续运行程序
+
 
 def create_gui(stats):
     root = tk.Tk()
@@ -266,6 +270,7 @@ def create_gui(stats):
 
     # 运行GUI循环
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
